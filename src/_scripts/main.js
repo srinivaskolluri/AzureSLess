2fb2fc512eedd08c2a990339a81c8fa29761439c
srinivas.kolluri <srinivas.kolluri@pwc.com>
2018-09-30 18:15:47 -0500

    Initial CheckIn

---
+++ src/_scripts/main.js
@@ -0,0 +1,14 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+import $ from 'jquery';
+import Link from '../_modules/link/link';
+
+$(() => {
+  new Link(); // Activate Link modules logic
+  console.log('Welcome to OrgChart!');
+
+  alert('Welcome to OrgChart!!');
+});
